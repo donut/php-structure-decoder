85c0c49a46612ae4b48fd73fae1eb32893d05c98
Donovan Mueller <zotobi@gmail.com>
2020-03-13 11:08:15 -0700

    feat: add dict_of_X decoders

--- src/types.php
+++ src/types.php
@@ -8,6 +8,7 @@ namespace RightThisMinute\StructureDecoder\types;
 use RightThisMinute\StructureDecoder\exceptions\DecodeError;
 use RightThisMinute\StructureDecoder\exceptions\WrongType;
 use function Functional\map;
+use function Functional\none;
 
 function string () : callable
 {
@@ -52,6 +53,43 @@ function array_of (callable $decoder) : callable
     $array = array_of_mixed()($value);
 
     return map($array, function($value, $key)use($decoder){
+      try {
+        return $decoder($value);
+      }
+      catch (\Throwable $exn) {
+        throw new DecodeError($value, $exn, (string)$key);
+      }
+    });
+  };
+}
+
+
+function dict_of_mixed () : callable
+{
+  return function ($value) : array
+  {
+    if (is_object($value))
+      $value = get_object_vars($value);
+    else if (!is_array($value))
+      throw new WrongType($value, 'object or associative array');
+
+    $is_associative_array =
+      none($value, function ($_, $key){ return !is_string($key); });
+    if (!$is_associative_array)
+      throw new WrongType
+        ($value, 'associative array with string keys');
+
+    return $value;
+  };
+}
+
+
+function dict_of (callable $decoder) : callable
+{
+  return function ($value) use ($decoder) : array
+  {
+    $dict = dict_of_mixed()($value);
+    return map($dict, function($value, $key)use($decoder){
       try {
         return $decoder($value);
       }
